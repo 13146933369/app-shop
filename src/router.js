22d82b94992dc8880644714c77c6863c67922b65
vue-sell <949471818@qq.com>
2019-05-05 17:57:15 +0800

    first commit

--- src/router.js
+++ src/router.js
@@ -1,27 +1,18 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Goods from "./components/goods/goods"
+import Ratings from "./components/ratings/ratings"
+import Seller from "./components/seller/seller"
 
 Vue.use(Router);
 
 export default new Router({
+  linkActiveClass:"active",
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function() {
-        return import(/* webpackChunkName: "about" */ "./views/About.vue");
-      }
-    }
+    {path: "/goods", name: "goods",component:Goods},
+    {path: "/ratings", name: "ratings",component:Ratings},
+    {path: "/seller", name: "seller",component:Seller},
   ]
 });
